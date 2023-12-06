4bd41f396620df2068a72a19c815a71d57e2fe62
LirioMendoza <aketzalirio@gmail.com>
2023-12-06 12:36:15 -0600

    FEAT(comments-id[GET/PUT/DELETE]): Applying get, put and delete methods for comments-id

--- routes/comment_routes.py
+++ routes/comment_routes.py
@@ -15,7 +15,9 @@ class CommentRoutes(Blueprint):
     def commentRoutes(self):
         self.route('/api/resources/<int:resource_id>/comments', methods=['GET'])(self.getComments)
         self.route('/api/resources/<int:resource_id>/comments', methods=['POST'])(self.addComment)
-
+        self.route('/api/resources/comments/<int:comment_id>', methods=['GET'])(self.getCommentById)
+        self.route('/api/resources/comments/<int:comment_id>', methods=['PUT'])(self.updateComment)
+        self.route('/api/resources/comments/<int:comment_id>', methods=['DELETE'])(self.deleteComment)
 
     def getComments(self, resource_id):
         try:
@@ -54,3 +56,55 @@ class CommentRoutes(Blueprint):
             return jsonify(self.created_comment), 201
         except Exception as e:
             log.critical(f'Error adding a new comment to the database: {e}')
+
+    def getCommentById(self, comment_id):
+        self.comment = self.comment_service.getCommentById(comment_id)
+        if self.comment:
+            return jsonify(self.comment), 200
+        else: 
+            return jsonify({'error': 'Comment not found'}), 404
+            
+    def updateComment(self, comment_id):
+        try:
+            self.data = request.json
+            if not self.data:
+                return jsonify({'error': 'Invalid data'}), 400
+            
+             # Updates a Resource attributes 
+
+            self.title = self.data.get('title')
+            self.comment = self.data.get('comment')
+            self.rating = self.data.get('rating')
+
+            try: # Validations for the data
+                if self.title:
+                    self.comment_schema.validateTitle(self.title)
+
+                if self.comment:
+                    self.comment_schema.validateComment(self.comment)
+
+                if self.rating:
+                    self.comment_schema.validateRating(self.rating)
+
+            except ValidationError as e:
+                 return(jsonify({'error': 'Invalid data', 'details': e.messages}), 400)
+            
+            self.comment_updated = self.comment_service.updateComment(comment_id, self.data)
+
+            if self.comment_updated:
+                return jsonify(self.comment_updated), 200
+            else:
+                return jsonify({'error': 'Comment not found'}), 404
+
+        except Exception as e:
+            log.critical(f'Error updating the comment in the database: {e}')
+
+    def deleteComment(self, comment_id):
+        try:
+            self.comment_deleted = self.comment_service.deleteComment(comment_id)
+            if self.comment_deleted:
+                return jsonify(self.comment_deleted), 200
+            else:
+                return jsonify({'error': 'Comment not found'}), 404
+        except Exception as e:
+            log.critical(f'Error deleting the comment in the database: {e}')
