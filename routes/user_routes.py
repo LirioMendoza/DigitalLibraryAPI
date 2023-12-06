1556abfbc0325e44e4791155057e0b2384af4611
ClaraLuzCA <clara.cornejo@ingenieria.unam.edu>
2023-12-06 16:17:48 -0600

    FIX(user_id): add user_id.

--- routes/user_routes.py
+++ routes/user_routes.py
@@ -13,25 +13,27 @@ class UserRoutes(Blueprint):
     # Function where the routes are defined.
     def userRoutes(self):
         self.route('/api/sign-up', methods=['POST'])(self.addUser)
-        self.route('/api/profile/<int:user-id>', methods=['GET'])(self.getUser)
+        self.route('/api/profile/<int:user_id>', methods=['GET'])(self.getUser)
         self.route('/api/profile/<int:user_id>', methods=['PUT'])(self.updateUser)
         self.route('/api/profile/<int:user_id>', methods=['DELETE'])(self.deleteUser)
     
-     def addUser(self):
+    def addUser(self):
         try:
             self.data = request.json
             if not self.data:
                 return jsonify({'error': 'Invalid data'}), 400
             
+            self.user_id = self.data.get('user_id')
             self.user_name = self.data.get('user_name')
             self.first_name = self.data.get('first_name')
             self.last_name = self.data.get('last_name')
             self.email = self.data.get('email')
             self.avatar = self.data.get('avatar')
-            sel.password = self.data.get('password')
+            self.password = self.data.get('password')
 
 
             try: # Validations for the data
+                self.user_id.valiteUser_id(self.user_id)
                 self.user_schema.validateUser_name(self.user_name)
                 self.user_schema.validateFirst_name(self.first_name)
                 self.user_schema.validateLast_name(self.last_name)
@@ -39,10 +41,11 @@ class UserRoutes(Blueprint):
                 self.user_schema.validatePassword(self.password)
 
             except ValidationError as e:
-                 return(jsonify({'error': 'Invalid data', 'details': e.messages}), 400)
+                 return(jsonify({'error': 'Invalid data for user', 'details': e.messages}), 400)
             
             # Add a new User with its attributes 
             self.new_user = {
+                'user_id':self.user_id,
                 'user_name': self.user_name,
                 'first_name': self.first_name,
                 'last_name': self.last_name,
@@ -72,30 +75,33 @@ class UserRoutes(Blueprint):
                 return jsonify({'error': 'Invalid data'}), 400
             
              # Updates a User attributes 
-
+            self.user_id = self.data.get('user_id')
             self.user_name = self.data.get('user_name')
             self.first_name = self.data.get('first_name')
             self.last_name = self.data.get('last_name')
             self.email = self.data.get('email')
             self.avatar = self.data.get('avatar')
-            sel.password = self.data.get('password')
+            self.password = self.data.get('password')
 
 
             try: # Validations for the data
+                if self.user_id:
+                    self.user_schema.validateUserId(self.user_id)
+                    
                 if self.user_name:
-                    self.resource_schema.validateUser_name(self.user_name)
+                    self.user_schema.validateUser_name(self.user_name)
 
                 if self.first_name:
-                    self.resource_schema.validateFirst_name(self.first_name)
+                    self.user_schema.validateFirst_name(self.first_name)
 
                 if self.last_name:
-                    self.resource_schema.validateLast_name(self.last_name)
+                    self.user_schema.validateLast_name(self.last_name)
 
                 if self.email:
-                    self.resource_schema.validateEmail(self.email)
+                    self.user_schema.validateEmail(self.email)
                 
                 if self.password:
-                    self.resource_schema.validatePassword(self.password)
+                    self.user_schema.validatePassword(self.password)
 
                 
 
